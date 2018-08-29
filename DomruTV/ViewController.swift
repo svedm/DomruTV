d3266e0cc7dc8cb53f3acf2dd7a2895fda53fb08
Svetoslav <venom008@gmail.com>
2018-08-29 23:38:37 +0300

    Skeleton project

---
+++ DomruTV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DomruTV
+//
+//  Created by Svetoslav Karasev on 29/08/2018.
+//  Copyright © 2018 Svetoslav Karasev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
